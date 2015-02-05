7e857711bb437fa5102485a5efc75ea4e1edfb23
Filip Fiolka <fiolkaf@gmail.com>
2015-02-05 22:26:01 +0100

    Expose ObservableObject

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Observable = require('./src/mixin/observable');
 var Disposable = require('./src/mixin/disposable');
 var RemoteObject = require('./src/sync/remoteObject');
+var ObservableObject = require('./src/sync/observableObject');
 
 module.exports = {
     RemoteObject: RemoteObject,
